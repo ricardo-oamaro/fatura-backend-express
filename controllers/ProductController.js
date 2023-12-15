88e4573d5f942481bad192e2af6953ae7f08808c
Ricardo <ricardo.oamaro@gmail.com>
2023-12-15 10:51:17 -0300

    refactor do crud

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,21 +1,20 @@
 const Product = require('../models/Product')
 
 const getProducts = async (req, res) => {
-    const { userId } = req.body;
+    try {
+        console.log('logando o user ' + userId);
+        const products = await Product.find({ userId: userId })
 
-    const product = await Product.find({ userId })
-
-    if (!product){
-        res.status(404).json({ errors: ["Nenhum produto encontrado"]})
-        return;
+        if (!products){
+            res.status(404).json({ errors: ["Nenhum produto encontrado"]})
+            return;
+            
+        }
+        res.status(200).json({products})
+    } catch (error) {
+        console.log(error);
     }
 
-    res.status(200).json({ 
-        date: product.date,
-        description: product.description,
-        value: product.value
-
-    })
 }
 
 const createProduct = async (req, res) => {
@@ -38,7 +37,39 @@ const createProduct = async (req, res) => {
     }
 }
 
+const updateProduct = async (req, res) => {
+    const { _id, date, description, value } = req.body
+
+    try {
+        // Encontra o registro pelo ID e atualiza no banco de dados
+        const produtoAtualizado = await Product.findByIdAndUpdate(_id, { date: date, description: description, value: value }, { new: true });
+
+        res.status(201).json(produtoAtualizado);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+const deleteProduct = async (req, res) => {
+    try {
+        const id = req.params.id
+        console.log('O ID é ' + id);
+        
+        const produtoExcluido = await Product.findByIdAndDelete(id);
+
+        if (!produtoExcluido) {
+            return res.status(404).json({ message: 'Produto não encontrado.' });
+        }
+
+        res.json({ message: 'Produto excluído com sucesso.' });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
 module.exports = {
     getProducts,
-    createProduct
+    createProduct,
+    updateProduct,
+    deleteProduct
 }
--- routes/ProductRoutes.js
+++ routes/ProductRoutes.js
@@ -2,11 +2,13 @@ const express = require("express");
 const router = express.Router();
 const getUserId = require("../middleware/getUserId")
 
-const { getProducts, createProduct } = require('../controllers/ProductController')
+const { getProducts, createProduct, updateProduct, deleteProduct } = require('../controllers/ProductController')
 
 const validate = require("../middleware/handleValidation");
 
-router.get('/get-products', validate, getProducts)
+router.get('/get-products', getUserId, validate, getProducts)
 router.post('/create', getUserId, validate, createProduct)
+router.put('/update/:id', getUserId, validate, updateProduct)
+router.delete('/delete/:id', validate, deleteProduct)
 
 module.exports = router;
